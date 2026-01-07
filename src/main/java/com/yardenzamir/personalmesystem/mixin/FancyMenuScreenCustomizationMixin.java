93e787b34d1d721db56dc5ee15b5c923e725b166
Yarden Zamir <dev@yarden-zamir.com>
2026-01-07 18:42:04 +0200

    feat: add create blacklist removal

--- src/main/java/com/yardenzamir/personalmesystem/mixin/FancyMenuScreenCustomizationMixin.java
+++ src/main/java/com/yardenzamir/personalmesystem/mixin/FancyMenuScreenCustomizationMixin.java
@@ -6,14 +6,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
- * Removes AE2 screens from FancyMenu's blacklist so they can be customized.
+ * Removes AE2 and Create screens from FancyMenu's blacklist so they can be customized.
  */
 @Mixin(targets = "de.keksuccino.fancymenu.customization.ScreenCustomization", remap = false)
 public class FancyMenuScreenCustomizationMixin {
 
     @Inject(method = "isScreenBlacklisted(Ljava/lang/String;)Z", at = @At("HEAD"), cancellable = true)
-    private static void allowAE2Screens(String screenClassPath, CallbackInfoReturnable<Boolean> cir) {
-        if (screenClassPath != null && screenClassPath.startsWith("appeng.")) {
+    private static void allowModScreens(String screenClassPath, CallbackInfoReturnable<Boolean> cir) {
+        if (screenClassPath != null &&
+                (screenClassPath.startsWith("appeng.") || screenClassPath.startsWith("com.simibubi.create."))) {
             cir.setReturnValue(false);
         }
     }
